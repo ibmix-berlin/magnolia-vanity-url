5b3ae4f6c9614f6b655a1849e9eab56fa63dab8b
Marvin Kerkhoff <marvin.kerkhoff@arvato-systems.ch>
2015-10-13 13:40:03 +0200

    Folder Definition

--- src/main/java/com/aperto/magnolia/vanity/setup/VanityUrlModuleVersionHandler.java
+++ src/main/java/com/aperto/magnolia/vanity/setup/VanityUrlModuleVersionHandler.java
@@ -26,9 +26,8 @@ package com.aperto.magnolia.vanity.setup;
 import info.magnolia.jcr.nodebuilder.task.NodeBuilderTask;
 import info.magnolia.module.DefaultModuleVersionHandler;
 import info.magnolia.module.InstallContext;
-import info.magnolia.module.delta.BootstrapConditionally;
-import info.magnolia.module.delta.DeltaBuilder;
-import info.magnolia.module.delta.Task;
+import info.magnolia.module.delta.*;
+import info.magnolia.repository.RepositoryConstants;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -46,6 +45,7 @@ import static info.magnolia.repository.RepositoryConstants.CONFIG;
  * @author frank.sommer
  */
 public class VanityUrlModuleVersionHandler extends DefaultModuleVersionHandler {
+    private static final String FLUSH_CACHE_PATH = "/modules/cache/config/configurations/default/flushPolicy/policies/flushAll/repositories";
 
     private final Task _addAppToLauncher = new NodeBuilderTask("Add app to app launcher", "Add vanity url app to app launcher.", logging, CONFIG, "/modules/ui-admincentral/config/appLauncherLayout/groups/manage/apps",
         addNode("vanityUrl", ContentNode.NAME)
@@ -59,10 +59,21 @@ public class VanityUrlModuleVersionHandler extends DefaultModuleVersionHandler {
         )
     );
 
+    private final Task _addCacheFlushConfig = new PropertyExistsDelegateTask("Check cache flush", "Check cache flush config and add missing configuration.",
+        RepositoryConstants.CONFIG, FLUSH_CACHE_PATH, WORKSPACE, null,
+        new NodeBuilderTask("Add flush config", "Add flush configuration for vanity url workspace.", logging, CONFIG, FLUSH_CACHE_PATH,
+            addProperty(WORKSPACE, WORKSPACE)
+        )
+    );
+
     public VanityUrlModuleVersionHandler() {
         DeltaBuilder update131 = DeltaBuilder.update("1.3.1", "Update to version 1.3.1");
         update131.addTask(new BootstrapConditionally("Bootstrap new config", "Bootstrap new public url service configuration.", "/mgnl-bootstrap/magnolia-vanity-url/config.modules.magnolia-vanity-url.config.publicUrlService.xml"));
         register(update131);
+        
+        DeltaBuilder update133 = DeltaBuilder.update("1.3.3", "Update to version 1.3.3");
+        update133.addTask(new BootstrapConditionally("Bootstrap new config", "Bootstrap folder definition in app.", "/mgnl-bootstrap/magnolia-vanity-url/config.modules.magnolia-vanity-url.apps.vanityUrl.xml"));
+        register(update133);
     }
 
     @Override
@@ -70,6 +81,7 @@ public class VanityUrlModuleVersionHandler extends DefaultModuleVersionHandler {
         List<Task> tasks = new ArrayList<>();
         tasks.add(_addAppToLauncher);
         tasks.add(_addUriRepositoryMapping);
+        tasks.add(_addCacheFlushConfig);
         return tasks;
     }
 }
